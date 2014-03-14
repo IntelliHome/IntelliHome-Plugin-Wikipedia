4a66072ff6a55956842dd5346a88a8d630da43b1
mudler <mudler@dark-lab.net>
2014-03-14 15:00:57 +0100

    modifying pod

--- lib/IH/Plugin/Wikipedia.pm
+++ lib/IH/Plugin/Wikipedia.pm
@@ -1,10 +1,60 @@
 package IH::Plugin::Wikipedia;
 
+
+=encoding utf-8
+
+=head1 NAME
+
+IH::Plugin::Wikipedia - Wikipedia plugin for Google@Home
+
+=head1 SYNOPSIS
+
+  $ ./intellihome-master -i Wikipedia #for install
+  $ ./intellihome-master -r Wikipedia #for remove
+
+=head1 DESCRIPTION
+
+IH::Plugin::Wikipedia is a wikipedia plugin that enables searches on wikipedia by calling "Wikipedia <term>" on the interfaces supported by Google@Home
+
+=head1 METHODS
+
+=over
+
+=item search
+
+Takes input terms and process the Wikipedia research.
+It reads coniguration from Config attribute (If you intend to use it separately to G@H you need to setup it properly).
+The output is redirected to the parser output (to be dispatched on the correct node).
+
+=item install
+Install the plugin into the mongo Database
+
+=item remove
+Remove the plugin triggers from the mongo Database
+
+=head1 AUTHOR
+
+mudler E<lt>mudler@dark-lab.netE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- mudler
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+L<WWW::Wikipedia>, L<WWW::Google::AutoSuggest>
+
+=cut
+
+
 use strict;
 use 5.008_005;
 our $VERSION = '0.01';
 use Moose;
-use IH::Schema::Mongo::Trigger;
 use WWW::Google::AutoSuggest;
 use WWW::Wikipedia;
 use Encode;
@@ -45,7 +95,7 @@ sub search {
 
     }
     if ($Output) {
-        $Output = $hs->parse( $Output );
+        $Output = $hs->parse($Output);
         $Output =~ s/\n/ /g;
         local $/;
         $Output =~ s/\{.*?\}|\[.*?\]|\(.*?\)/ /g;
@@ -65,7 +115,7 @@ sub install {
             plugin        => "Wikipedia",
             plugin_method => "search"
         }
-    );
+    ) if $self->Parser->Backend->isa("IH::Parser::DB::Mongo");
 
 }
 
@@ -76,43 +126,8 @@ sub remove {
             plugin        => "Wikipedia",
             plugin_method => "search"
         }
-    );
-}
-
-sub update {
-
+    ) if $self->Parser->Backend->isa("IH::Parser::DB::Mongo");
 }
 
 1;
 __END__
-
-=encoding utf-8
-
-=head1 NAME
-
-IH::Plugin::Wikipedia - Blah blah blah
-
-=head1 SYNOPSIS
-
-  use IH::Plugin::Wikipedia;
-
-=head1 DESCRIPTION
-
-IH::Plugin::Wikipedia is
-
-=head1 AUTHOR
-
-mudler E<lt>mudler@dark-lab.netE<gt>
-
-=head1 COPYRIGHT
-
-Copyright 2014- mudler
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 SEE ALSO
-
-=cut
